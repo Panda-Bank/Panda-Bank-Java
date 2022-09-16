4b36711e54b595e52af10cffa8b3cf933838baad
israel865974@gmail.com <israel.bsilva8@senacsp.edu.br>
2022-09-15 21:25:38 -0300

    Subindo projeto

---
+++ src/main/java/com/senac/panda/PandaApplication.java
@@ -0,0 +1,13 @@
+package com.senac.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/panda/PandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.panda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
