471e6c2221899b6ff881b91726f3a8850957bc9d
vinicius <viniciuscarvalho134@gmail.com>
2022-10-15 19:30:50 -0300

    AtulizandoPix

--- src/main/java/com/senac/panda/model/Pix.java
+++ src/main/java/com/senac/panda/model/Pix.java
@@ -1,5 +1,25 @@
 package com.senac.panda.model;
 
-public class Pix {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "pix")
+public class Pix {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_pix;
+	
+	@NotBlank
+	private String chave;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "fk_usuario_conta_id_usuario")
+//	private long fk_usuario_conta_id_usuario;
+	
 }
