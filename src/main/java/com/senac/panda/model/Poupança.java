d46dd5daaa2d0ff09a9f60f0c96f82b20c854434
israel bernardo <israel865974@gmail.com>
2022-10-20 21:21:57 -0300

    Avaliações

--- src/main/java/com/senac/panda/model/Poupança.java
+++ src/main/java/com/senac/panda/model/Poupança.java
@@ -1,5 +1,29 @@
 package com.senac.panda.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Poupança")
 public class Poupança {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_Poupança;
+	
+	@NotBlank (message = "Poupança não pode estar vazia")
+	private String Poupança;
+	
+    @ManyToOne
+	@JoinColumn(name = "fk_usuario_conta_id_usuario")
+    private long fk_usuario_conta_id_usuario;
+    
+	
 }
