a9216b674a12bf718e06c3e3c7c7db9e61677770
Vinicius <viniciuscarvalho134@gmail.com>
2022-10-27 22:38:28 -0300

    atualizacao back-end

--- src/main/java/com/senac/panda/model/Avaliacoes.java
+++ src/main/java/com/senac/panda/model/Avaliacoes.java
@@ -4,17 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name = "avaliacoes")
+@Table(name = "avaliacao")
 public class Avaliacoes {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id_comentario ;
+	private long id_avaliacao ;
 	
 	@NotBlank(message = "campo classificacao nao pode ser em branco")
 	@Size(min = 5, max = 50)
@@ -24,5 +28,34 @@ public class Avaliacoes {
 	@Size(min = 2, max = 40)
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "fk_usuario")
+	@JsonIgnoreProperties("avaliacoes")
+	private Usuario usuarios;
 
+	public String getClassificacao() {
+		return classificacao;
+	}
+
+	public void setClassificacao(String classificacao) {
+		this.classificacao = classificacao;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Usuario getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(Usuario usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	
 }
--- src/main/java/com/senac/panda/model/Usuario.java
+++ src/main/java/com/senac/panda/model/Usuario.java
@@ -1,5 +1,203 @@
 package com.senac.panda.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "usuario_conta")
 public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "campo classificacao nao pode ser em branco")
+	@Size(min = 5, max =50)
+	private String nome;
+	
+	@NotBlank (message = "campo classificacao nao pode ser em branco")
+	@Size(min = 7, max = 20 )
+	private String cpf;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data_nascimento;
+	
+	@NotBlank (message = "campo email não pode ser em branco")
+	@Size(min = 10, max = 50)
+	private String email;
+	
+	@NotBlank (message = "campo senha não pode ser em branco")
+	@Size(min = 8, max = 20)
+	private String senha;
+	
+	@NotBlank (message = "campo telefone não pode ser em branco")
+	@Size(min = 8, max = 15)
+	private String telefone;
+	
+	@NotBlank (message = "campo logradouro não pode ser em branco")
+	@Size(min = 8, max = 30)
+	private String logradouro;
+	
+	@NotBlank (message = "campo CEP não pode ser em branco")
+	@Size(min = 8, max = 9)
+	private String cep;
+	
+	@NotBlank (message = "campo numero enderenço não pode ser em branco")
+	@Size(min = 8, max = 20)
+	private String numero_endereco;
+	
+	@Size(min = 8, max = 20)
+	private String complemento;
+	
+	@SuppressWarnings("unused")
+	private double saldo = 600.0;
+	
+	@NotBlank (message = "campo agencia não pode ser em branco")
+	@Size(min = 4, max = 4)
+	private String agencia;
+	
+	@NotBlank (message = "campo conta não pode ser em branco")
+	@Size(min = 10, max = 10)
+	private String conta;
+	
+	@OneToMany(mappedBy ="usuarios", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({"usuarios"})
+	private List<Avaliacoes> avaliacoes = new ArrayList<>();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getNumero_endereco() {
+		return numero_endereco;
+	}
+
+	public void setNumero_endereco(String numero_endereco) {
+		this.numero_endereco = numero_endereco;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getConta() {
+		return conta;
+	}
+
+	public void setConta(String conta) {
+		this.conta = conta;
+	}
+
+	public List<Avaliacoes> getAvaliacoes() {
+		return avaliacoes;
+	}
+
+	public void setAvaliacoes(List<Avaliacoes> avaliacoes) {
+		this.avaliacoes = avaliacoes;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
