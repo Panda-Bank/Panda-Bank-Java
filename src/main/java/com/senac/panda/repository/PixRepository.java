a2d568e998f5df83e76429591e62e6aecfce7fc0
matheus <matheus.malmeida7@gmail.com>
2022-11-10 15:59:19 -0300

    Atualizando Pix

--- src/main/java/com/senac/panda/repository/PixRepository.java
+++ src/main/java/com/senac/panda/repository/PixRepository.java
@@ -1,15 +1,21 @@
 package com.senac.panda.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.senac.panda.model.Pix;
-import com.senac.panda.model.Usuario;
 
 @Repository
 public interface PixRepository extends JpaRepository<Pix, Long>{
 	
-	public Optional<Pix> findByChaveAndTipoChave(String chave, String tipo_chave);
+	public Optional<Pix> findByChaveAndTipo(String chave, String tipo);
+
+	public Optional<Pix> findById(long id);
+	
+	public Optional<Pix> findByChave(String chave);
+	
+	public List<Pix> findAllByUsuariosId(long fk);
 }
